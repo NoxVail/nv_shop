81b20ce817d50b2a17f319a94583d65fbde5f469
Ihor Bohdan <warden876@gmail.com>
2022-03-11 14:52:36 +0200

    variant creation fix

--- lib/interactors/webhooks/variant_create.rb
+++ lib/interactors/webhooks/variant_create.rb
@@ -24,8 +24,8 @@ class Interactors::Webhooks::VariantCreate
   def create_variant
     context.params[:variants].each do |variant_hash|
       variant_data = variant_hash.slice(*DIRECT_FIELDS)
-      FIELD_MAP.each { |k, v| variant_data[k] = context.params[v] }
-      variant = context.product.variants.create_with(variant_data).find_or_initialize_by(shopify_id: variant_data[:shopify_id], title: variant_data[:title])
+      FIELD_MAP.each { |k, v| variant_data[k] = variant_hash[v] }
+      variant = context.product.variants.create_with(variant_data).find_or_initialize_by(shopify_id: variant_data[:shopify_id])
       failed(variant.errors.full_messages) unless variant.save
     end
   end
